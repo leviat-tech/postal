2ee3fe5df19bc2adae966762f7786acdedff1af1
Stephen <squebe@crh.io>
2020-02-04 14:59:04 -0700

    Remove debug print statement

--- postal/utils.py
+++ postal/utils.py
@@ -3,7 +3,6 @@ from subprocess import call
 
 
 def shell(command, silent=False):
-    print('Execute', command)
     if silent:
         with open(os.devnull, 'w') as DEVNULL:
             return call(command, shell=True, stdout=DEVNULL, stderr=DEVNULL) == 0
