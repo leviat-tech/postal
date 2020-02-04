700a2e30b4a3c982d533b5511b9d44cf8fdc6d87
Stephen <squebe@crh.io>
2020-02-03 22:13:22 -0700

    Create cli compose, enter, and launch commands

---
+++ postal/utils.py
@@ -0,0 +1,11 @@
+import os
+from subprocess import call
+
+
+def shell(command, silent=False):
+    print('Execute', command)
+    if silent:
+        with open(os.devnull, 'w') as DEVNULL:
+            return call(command, shell=True, stdout=DEVNULL, stderr=DEVNULL) == 0
+    else:
+        return call(command, shell=True) == 0
