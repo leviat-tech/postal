184f4fd1f579bca3f0f2f280301ac05ac3764253
Stephen <squebe@crh.io>
2020-03-09 13:03:14 -0600

    Raise an unimplemented  exception when you try to deploy from git

--- postal/core/swarm.py
+++ postal/core/swarm.py
@@ -5,7 +5,7 @@ from .utils import shell
 def proxy(args):
     return shell(f'docker {" ".join(args)}')
 
-def deploy(stack, dir=None, repo=None):
+def deploy(stack, dir=None, branch=None):
     if dir:
         compose = 'stack/production.yml'
         with open(os.path.join(dir, 'stack/production.env'), 'w') as f:
@@ -16,3 +16,6 @@ def deploy(stack, dir=None, repo=None):
         shell(f'cd {dir} && docker-compose -p {stack} -f {compose} push')
         shell(f'cd {dir} && docker stack deploy --with-registry-auth -c {compose} {stack}')
         shell(f'rm -rf {dir}', silent=True)
+    else:
+        raise Exception("Deploying from github is not yet implemented.")
+        # shell('git clone')
