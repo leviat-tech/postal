efe805b293c73b6879c297b744dec9cde55c5265
Stephen <squebe@crh.io>
2020-03-10 21:31:51 -0600

    Launch a postal container in dev for better parity with production

--- stack/dev/enter.sh
+++ stack/dev/enter.sh
@@ -11,12 +11,6 @@ if ! id "$1" > /dev/null 2>&1; then
     # set default workdir
     su $1 -c 'echo "cd /postal" >> ~/.bashrc'
 
-    # set env vars so remote ssh calls will work
-    echo POSTAL_AWS_BUCKET=$POSTAL_AWS_BUCKET >> /etc/environment
-    echo POSTAL_AWS_REGION=$POSTAL_AWS_REGION >> /etc/environment
-    echo AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID >> /etc/environment
-    echo AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY >> /etc/environment
-
     # add user to docker group
     usermod -aG docker $1
 
@@ -27,6 +21,7 @@ if ! id "$1" > /dev/null 2>&1; then
     su $1 -c "ssh-keygen -t rsa -b 4096 -N '' -f ~/.ssh/id_rsa"
     su $1 -c "cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys"
     su $1 -c "chmod 600 ~/.ssh/authorized_keys"
+    cat /home/$1/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
 fi
 
 # setup shell
