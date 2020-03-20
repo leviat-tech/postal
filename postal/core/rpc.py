f59302af9861ac421c4102a48cb3ad51e174d500
Stephen <squebe@crh.io>
2020-03-20 14:36:37 -0600

    Use rsync to send files to postal faster

--- postal/core/rpc.py
+++ postal/core/rpc.py
@@ -44,7 +44,7 @@ class Proxy:
     def send(self, source):
         destination = f'/tmp/postal_stack_{uuid.uuid1()}'
         print('Uploading stack...', end=' ')
-        if shell(f"scp -P {self.port} -r {source} {self.user}@{self.host}:{destination}"):
+        if shell(f'rsync -a --info=progress2 -e "ssh -p {self.port}" {source}/* {self.user}@{self.host}:{destination}'):
             print('Done.')
         else:
             print('Failed')
