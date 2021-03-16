acea3978b1daf7b10e0ea3eb6a5f6a96b315c1fd
Stephen <squebe@crh.io>
2021-03-16 13:05:05 -0600

    Fix error in postal version command

--- postal/commands/version.py
+++ postal/commands/version.py
@@ -1,6 +1,6 @@
 try:
     from importlib import metadata
-except ImportError: # for Python<3.8
+except (ImportError, ModuleNotFoundError):
     import importlib_metadata as metadata
 
 help = "Show current postal version"
